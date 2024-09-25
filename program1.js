7406a8def961e667dd00b858d7d407eae6e55267
ayushinagar2004 <ayushinagar2004@gmail.com>
2024-09-25 13:53:35 +0530

    Update

--- program1.js
+++ program1.js
@@ -1,8 +1,48 @@
 const getTotalIsles = function (grid) {
+  if (!grid || grid.length === 0) return 0;
 
+  const rows = grid.length;
+  const cols = grid[0].length;
+  let islandCount = 0;
 
-  // write your code here
+  const dfs = (r, c) => {
+      if (r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] !== 'L') {
+          return;
+      }
+      grid[r][c] = 'W';
+      dfs(r + 1, c);
+      dfs(r - 1, c);
+      dfs(r, c + 1);
+      dfs(r, c - 1);
+  };
 
+  for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+          if (grid[i][j] === 'L') {
+              islandCount++;
+              dfs(i, j);
+          }
+      }
+  }
+
+  return islandCount;
 };
+
+const map1 = [
+  ["L", "L", "L", "L", "W"],
+  ["L", "L", "W", "L", "W"],
+  ["L", "L", "W", "W", "W"],
+  ["W", "W", "W", "W", "W"],
+];
+
+const map2 = [
+  ["L", "L", "W", "W", "W"],
+  ["L", "L", "W", "W", "W"],
+  ["W", "W", "L", "W", "W"],
+  ["W", "W", "W", "L", "L"],
+];
+
+console.log(getTotalIsles(map1));
+console.log(getTotalIsles(map2)); 
 
 module.exports = getTotalIsles;
